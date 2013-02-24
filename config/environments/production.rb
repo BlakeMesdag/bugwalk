c528972202bd3a37cbe956284f52bd311d8b0ff9
Blake Mesdag <blakemesdag@gmail.com>
2013-02-23 20:24:38 -0500

    Place it in application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,6 @@ Bugwalk::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  config.assets.initialize_on_precompile = false
-
   config.after_initialize do
     Thread.new do
       gembeat_config = YAML.load_file("#{Rails.root.to_s}/config/gembeat.yml")
