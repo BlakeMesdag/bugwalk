5eb0f630e1e90055b6d005dd7a75f67ae35b12d3
Blake Mesdag <blakemesdag@gmail.com>
2013-02-23 20:22:58 -0500

    Send pulse in new thread and skip initialize on precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,11 +64,16 @@ Bugwalk::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.assets.initialize_on_precompile = false
+
   config.after_initialize do
-    gembeat_config = YAML.load_file("#{Rails.root.to_s}/config/gembeat.yml")
-    Gembeat.token = gembeat_config["token"]
-    Gembeat.pulse_url = gembeat_config["pulse_url"]
-    Gembeat.use_ssl = true
-    Gembeat.send_pulse
+    Thread.new do
+      gembeat_config = YAML.load_file("#{Rails.root.to_s}/config/gembeat.yml")
+      Gembeat.token = gembeat_config["token"]
+      Gembeat.pulse_url = gembeat_config["pulse_url"]
+      Gembeat.use_ssl = true
+      Gembeat.send_pulse
+    end
   end
 end
